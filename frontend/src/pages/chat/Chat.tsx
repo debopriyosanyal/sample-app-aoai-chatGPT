48f2217615ea4d14c854f17603bbd4ad236aafe8
ds <debopriyo.sanyal@gmail.com>
2024-04-04 16:16:38 +0530

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash-es";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Contoso from "../../assets/29918863.jpg";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
